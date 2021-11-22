8a409c3a7e33538014a636679fa88f8a528a8b5f
Sree Gauthami Gundaram <93358202+Sree-Gauthami-Gundaram@users.noreply.github.com>
2021-11-21 20:47:55 -0500

    Delete order_alter.sql

--- deliverable-2/new_tables_sql_scripts_del2/order_alter.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `Campus_Eats_Fall2020`.`order`
-ADD COLUMN `status_id` INT NOT NULL AFTER `delivery_charge`, 
-ADD COLUMN `timestamp` DATETIME NOT NULL AFTER `status_id`,
-CHANGE COLUMN `delivery_charge` `delivery_charge` FLOAT NOT NULL , 
-ADD INDEX `fk_O_status_id_idx` (`status_id` ASC) VISIBLE;
-ALTER TABLE `Campus_Eats_Fall2020`.`order`
-ADD CONSTRAINT `fk_O_status_id`
-FOREIGN KEY (`status_id`)
-REFERENCES `Campus_Eats_Fall2020`.`order_status` (`status_id`);
