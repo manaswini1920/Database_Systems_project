272dfb29e15b59cd789cedb0449a6326a08e301d
Jyosthna Gandhodi <gjyosthna71@gmail.com>
2021-12-08 21:19:37 -0500

    Delete Question4.sql

--- deliverable-3/Sql scripts/Question4.sql
+++
@@ -1,5 +0,0 @@
-SELECT o.person_id,food_rating,delivery_rating 
-FROM order_rating AS ora,`order` AS o
-where ora.order_id IN
-(SELECT order_id FROM `order`
-WHERE restaurant_id= 3 AND o.person_id = 3)
