1eff3b6779813054528d4b8bc87b3a82808b292d
Jyosthna Gandhodi <gjyosthna71@gmail.com>
2021-12-08 21:19:05 -0500

    Delete Question1.sql

--- deliverable-3/Sql scripts/Question1.sql
+++
@@ -1,5 +0,0 @@
-SELECT MAX(delivery_rating),MIN(delivery_rating),AVG(food_rating),
-MAX(food_rating),MIN(food_rating),AVG(food_rating)
-FROM order_rating
-WHERE order_id
-IN(SELECT order_id FROM `order` WHERE `order`.restaurant_id = 4);
